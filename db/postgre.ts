991b383052bb1ed9900c4f24cb8e1b94a7493c89
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2021-01-16 11:16:41 +0300

    postgresql support

--- db/postgre.ts
+++ db/postgre.ts
@@ -2,9 +2,7 @@ import * as initKnex from "knex";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-const { PG_USERNAME, PG_PASSWORD, PG_HOST, PG_PORT} = process.env;
-const dbName = "clonewarsDB";
-const url = `postgres://${PG_USERNAME}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${dbName}`;
+const url = process.env.DATABASE_URL
 
 const knex = initKnex({
   client: "pg",
